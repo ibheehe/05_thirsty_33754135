d8ecf198fe445912b9f4328ef7d92a37a50d5dff
ibheehe <ibrahimasif25@gmail.com>
2025-10-27 12:31:58 +0000

    commit

--- routes/main.js
+++ routes/main.js
@@ -19,6 +19,23 @@ router.get("/search", (req, res) => {
     res.render("search.ejs",shopData)
 }); 
 
+router.get('/search_result', function (req, res) {
+  const name = req.query.name_text;
+  const category = req.query.category_text;
+  
+  res.send("You searched for " + name + " in " + category);
+});
+
+
+
+router.get("/register", (req,res) => {
+    res.render("register.ejs",  shopData); 
+}); 
+ 
+router.post("/registered", (req,res) => { 
+  res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!'+ "we will send an email to you at: " + req.body.email); 
+}); 
+
 // Export the router object so index.js can access it
 module.exports = router;
 
