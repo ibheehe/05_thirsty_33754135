4244d9db005c49a0aeff348ec02e181998d72c24
ibheehe <ibrahimasif25@gmail.com>
2025-10-27 12:31:53 +0000

    first commit

---
+++ routes/main.js
@@ -0,0 +1,24 @@
+// Create a new router
+const express = require("express");
+const router = express.Router();
+
+// Define our data
+var shopData = {shopName: "The Thirsty Student", 
+                productCategories:["Beer", "Wine", "Soft Drinks", "Hot Drinks"]}
+
+// Handle the main routes
+router.get("/", (req, res) => {
+    res.render("index.ejs", shopData)
+}); 
+
+router.get("/about", (req, res) => {
+    res.render("about.ejs", shopData)
+}); 
+
+router.get("/search", (req, res) => {
+    res.render("search.ejs",shopData)
+}); 
+
+// Export the router object so index.js can access it
+module.exports = router;
+
